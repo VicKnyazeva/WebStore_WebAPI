755dbbdcf267bc1b77394f1d59f81b6a8e90648d
VicKnyazeva <victoria.knyazeva@hotmail.com>
2021-10-20 02:17:43 +0700

    Визуализация данных, полученных из WebAPI

---
+++ UI/WebStore/Controllers/WebAPIController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+using WebStore.Interfaces.TestAPI;
+
+namespace WebStore.Controllers
+{
+    public class WebAPIController : Controller
+    {
+        private readonly IValuesService _ValuesService;
+        public WebAPIController(IValuesService ValuesService)
+        {
+            _ValuesService = ValuesService;
+        }
+        public IActionResult Index()
+        {
+            var values = _ValuesService.GetAll();
+            return View(values);
+        }
+    }
+}
