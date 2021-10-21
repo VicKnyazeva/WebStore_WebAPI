797470e2cf858f4b01b8f955cde6600d9ce57696
VicKnyazeva <victoria.knyazeva@hotmail.com>
2021-10-22 02:40:01 +0700

    DTO товаров, секций, брендов + контроллер

---
+++ Common/WebStore.Domain/DTO/SectionDTOMapper.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using WebStore.Domain.Entities;
+
+namespace WebStore.Domain.DTO
+{
+    public static class SectionDTOMapper
+    {
+        public static SectionDTO ToDTO(this Section section) => section is null
+            ? null
+            : new SectionDTO
+            {
+                Id = section.Id,
+                Name = section.Name,
+                Order = section.Order,
+                ParentId = section.ParentId,
+            };
+
+        public static Section FromDTO(this SectionDTO section) => section is null
+            ? null
+            : new Section
+            {
+                Id = section.Id,
+                Name = section.Name,
+                Order = section.Order,
+                ParentId = section.ParentId,
+            };
+
+        public static IEnumerable<SectionDTO> ToDTO(this IEnumerable<Section> sections) => sections.Select(ToDTO);
+        public static IEnumerable<Section> FromDTO(this IEnumerable<SectionDTO> sections) => sections.Select(FromDTO);
+    }
+}
