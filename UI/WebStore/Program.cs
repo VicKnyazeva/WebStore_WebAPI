3b8fdc3099f0a6ba5ad45fb64a13bd98408dd300
VicKnyazeva <victoria.knyazeva@hotmail.com>
2021-10-31 21:44:01 +0700

    Жесткая настройка системы конфигурации

--- UI/WebStore/Program.cs
+++ UI/WebStore/Program.cs
@@ -2,6 +2,8 @@ using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 
 namespace WebStore
 {
@@ -19,6 +21,14 @@ namespace WebStore
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(host => host
                    .UseStartup<Startup>()
+                   //.ConfigureLogging((host, log) => log
+                   //.ClearProviders()
+                   //.AddDebug()
+                   //.AddConsole(c => c.IncludeScopes = true)
+                   //.AddEventLog()
+                   //.AddFilter("Microsoft", LogLevel.Information)
+                   //.AddFilter<ConsoleLoggerProvider>("Microsoft.EntityFrameworkCore", LogLevel.Warning)
+                   //)
                 )
             ;
     }
