e877437e2403f95072a571ec28e4c224d12c26e5
VicKnyazeva <victoria.knyazeva@hotmail.com>
2021-11-15 04:30:55 +0700

    feat: Пример использования Ajax

---
+++ UI/WebStore/Controllers/API/ConsoleController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers.API
+{
+    [ApiController, Route("api/[controller]")]
+    public class ConsoleController : ControllerBase
+    {
+        [HttpGet("clear")]
+        public void Clear() => Console.Clear();
+
+        [HttpGet("WriteLine")]
+        public void WriteLine(string Message) => Console.WriteLine(Message);
+    }
+}
